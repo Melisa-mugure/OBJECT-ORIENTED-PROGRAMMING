0ff8b797f7b3efc9179eef448e0f044f074b58e3
Mewiiarts <mewiimella05@gmail.com>
2025-11-26 16:09:28 +0300

    OOP

---
+++ Main.java
@@ -0,0 +1,249 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("\n=======================================================");
+        System.out.println("Dedan Kimathi University of Technology");
+        System.out.println("School of Computer Science and IT");
+        System.out.println("Department of Computer Science");
+        System.out.println("Academic Year: 2024/2025");
+        System.out.println("STUDENT MARKSHEET (SECOND YEAR RESULTS)");
+        System.out.println("=======================================================\n");
+
+        Scanner scanner = new Scanner(System.in);
+        String regNumber1;
+        String fullName1;
+        int ccs2207_1;
+        int ccs2208_1;
+        int ccs2209_1;
+        int ccs2210_1;
+        int ccs2211_1;
+        int ccs2215_1;
+        int igs2202_1;
+
+
+        System.out.println("----Student 1----");
+        System.out.print("Enter Reg Number: ");
+        regNumber1 = scanner.next();
+        scanner.nextLine();
+
+        System.out.print("Enter Full Name: ");
+        fullName1 = scanner.nextLine();
+
+        System.out.print("Enter CCS 2207: ");
+        ccs2207_1 = scanner.nextInt();
+
+        System.out.print("Enter CCS 2208: ");
+        ccs2208_1 = scanner.nextInt();
+
+        System.out.print("Enter CCS 2209: ");
+        ccs2209_1 = scanner.nextInt();
+
+        System.out.print("Enter CCS 2210: ");
+        ccs2210_1 = scanner.nextInt();
+
+        System.out.print("Enter CCS 2211: ");
+        ccs2211_1 = scanner.nextInt();
+
+        System.out.print("Enter CCS 2215: ");
+        ccs2215_1 = scanner.nextInt();
+
+        System.out.print("Enter IGS 2202: ");
+        igs2202_1 = scanner.nextInt();
+        float total_1 = ccs2207_1 + ccs2208_1+ ccs2209_1+ ccs2210_1+ ccs2215_1+ccs2215_1+igs2202_1;
+        float average_1 = total_1/7;
+        String status_1;
+        String grade_1;
+
+
+        String regNumber2 ;
+        String fullName2 ;
+        int ccs2207_2 ;
+        int ccs2208_2 ;
+        int ccs2209_2 ;
+        int ccs2210_2 ;
+        int ccs2211_2 ;
+        int ccs2215_2 ;
+        int igs2202_2 ;
+
+
+        System.out.println("----Student 2----");
+        System.out.print("Enter Reg Number: ");
+        regNumber2 = scanner.next();
+        scanner.nextLine();
+
+        System.out.print("Enter Full Name: ");
+        fullName2 = scanner.nextLine();
+
+        System.out.print("Enter CCS 2207: ");
+        ccs2207_2 = scanner.nextInt();
+
+        System.out.print("Enter CCS 2208: ");
+        ccs2208_2 = scanner.nextInt();
+
+        System.out.print("Enter CCS 2209: ");
+        ccs2209_2 = scanner.nextInt();
+
+        System.out.print("Enter CCS 2210: ");
+        ccs2210_2 = scanner.nextInt();
+
+        System.out.print("Enter CCS 2211: ");
+        ccs2211_2 = scanner.nextInt();
+
+        System.out.print("Enter CCS 2215: ");
+        ccs2215_2 = scanner.nextInt();
+
+        System.out.print("Enter IGS 2202: ");
+        igs2202_2 = scanner.nextInt();
+
+        float total_2 = ccs2207_2+ ccs2208_2+ ccs2209_2+ ccs2210_2+ ccs2211_2+ ccs2215_2+igs2202_2;
+        float average_2 =  total_2/7;
+        String status_2 ;
+        String grade_2 ;
+
+        String regNumber3 ;
+        String fullName3 ;
+        int ccs2207_3 ;
+        int ccs2208_3 ;
+        int ccs2209_3 ;
+        int ccs2210_3 ;
+        int ccs2211_3 ;
+        int ccs2215_3 ;
+        int igs2202_3 ;
+
+        System.out.println("----Student 3----");
+        System.out.print("Enter Reg Number: ");
+        regNumber3 = scanner.next();
+        scanner.nextLine();
+
+        System.out.print("Enter Full Name: ");
+        fullName3 = scanner.nextLine();
+
+        System.out.print("Enter CCS 2207: ");
+        ccs2207_3 = scanner.nextInt();
+
+        System.out.print("Enter CCS 2208: ");
+        ccs2208_3 = scanner.nextInt();
+
+        System.out.print("Enter CCS 2209: ");
+        ccs2209_3 = scanner.nextInt();
+
+        System.out.print("Enter CCS 2210: ");
+        ccs2210_3 = scanner.nextInt();
+
+        System.out.print("Enter CCS 2211: ");
+        ccs2211_3 = scanner.nextInt();
+
+        System.out.print("Enter CCS 2215: ");
+        ccs2215_3 = scanner.nextInt();
+
+        System.out.print("Enter IGS 2202: ");
+        igs2202_3 = scanner.nextInt();
+
+        float total_3 = ccs2207_3+ccs2208_3+ccs2209_3+ccs2210_3+ ccs2211_3+ccs2215_3+igs2202_3;
+        float average_3 =  total_3/7;
+        String status_3 ;
+        String grade_3 ;
+
+
+        String regNumber4 ;
+        String fullName4 ;
+        int ccs2207_4 ;
+        int ccs2208_4 ;
+        int ccs2209_4 ;
+        int ccs2210_4 ;
+        int ccs2211_4 ;
+        int ccs2215_4 ;
+        int igs2202_4 ;
+
+        System.out.println("----Student 4----");
+        System.out.print("Enter Reg Number: ");
+        regNumber4 = scanner.next();
+        scanner.nextLine();
+
+        System.out.print("Enter Full Name: ");
+        fullName4 = scanner.nextLine();
+
+        System.out.print("Enter CCS 2207: ");
+        ccs2207_4 = scanner.nextInt();
+
+
+        System.out.print("Enter CCS 2208: ");
+        ccs2208_4 = scanner.nextInt();
+
+        System.out.print("Enter CCS 2209: ");
+        ccs2209_4 = scanner.nextInt();
+
+        System.out.print("Enter CCS 2210: ");
+        ccs2210_4 = scanner.nextInt();
+
+        System.out.print("Enter CCS 2211: ");
+        ccs2211_4 = scanner.nextInt();
+
+        System.out.print("Enter CCS 2215: ");
+        ccs2215_4 = scanner.nextInt();
+
+        System.out.print("Enter IGS 2202: ");
+        igs2202_4 = scanner.nextInt();
+
+        float total_4 = ccs2207_4+ccs2208_4+ccs2209_4+ccs2210_4+ccs2211_4+ccs2215_4+igs2202_4;
+        float average_4 =  total_4/7;
+        String status_4 ;
+        String grade_4 ;
+
+
+        String regNumber5 ;
+        String fullName5 ;
+        int ccs2207_5 ;
+        int ccs2208_5 ;
+        int ccs2209_5 ;
+        int ccs2210_5 ;
+        int ccs2211_5 ;
+        int ccs2215_5 ;
+        int igs2202_5 ;
+
+        System.out.println("----Student 5----");
+        System.out.print("Enter Reg Number: ");
+        regNumber5 = scanner.next();
+        scanner.nextLine();
+
+        System.out.print("Enter Full Name: ");
+        fullName5 = scanner.nextLine();
+
+        System.out.print("Enter CCS 2207: ");
+        ccs2207_5 = scanner.nextInt();
+
+        System.out.print("Enter CCS 2208: ");
+        ccs2208_5 = scanner.nextInt();
+
+        System.out.print("Enter CCS 2209: ");
+        ccs2209_5 = scanner.nextInt();
+
+        System.out.print("Enter CCS 2210: ");
+        ccs2210_5 = scanner.nextInt();
+
+        System.out.print("Enter CCS 2211: ");
+        ccs2211_5 = scanner.nextInt();
+
+        System.out.print("Enter CCS 2215: ");
+        ccs2215_5 = scanner.nextInt();
+
+        System.out.print("Enter IGS 2202: ");
+        igs2202_5 = scanner.nextInt();
+        scanner.close();
+
+        float total_5 = ccs2207_5+ccs2208_5+ccs2209_5+ccs2210_5+ccs2211_5+igs2202_5+ccs2215_5;
+        float average_5 =  total_5/7;
+        String status_5 ;
+        String grade_5 ;
+        System.out.println("Student Score Sheet");
+        System.out.println("=================================================================================================================================================================================================================================");
+        System.out.println("| Reg No.\t\t| Full Name\t\t| CCS 2207\t| CCS 2208\t| CCS 2209\t| CCS 2210\t| CCS 2211\t| CCS 2215\t| IGS 2202\t| TOTAL\t\t| AVERAGE\t| GRADE\t\t| STATUS\t|");
+        System.out.println("=================================================================================================================================================================================================================================");
+        System.out.println("| " + regNumber1 + "\t| " + fullName1 + "\t| " + ccs2207_1 + "\t\t| " + ccs2208_1 + "\t\t| " + ccs2209_1 + "\t\t| " + ccs2210_1 + "\t\t| " + ccs2211_1 + "\t\t| " + ccs2215_1 + "\t\t| " + igs2202_1 + "\t\t| " + total_1 + "\t\t| " + average_1 + "\t\t| \t\t| \t\t|");
+        System.out.println("| " + regNumber2 + "\t| " + fullName2 + "\t| " + ccs2207_2 + "\t\t| " + ccs2208_2 + "\t\t| " + ccs2209_2 + "\t\t| " + ccs2210_2 + "\t\t| " + ccs2211_2 + "\t\t| " + ccs2215_2 + "\t\t| " + igs2202_2 + "\t\t| " + total_2 + "\t\t| " + average_2 + "\t\t| \t\t| \t\t|");
+        System.out.println("| " + regNumber3 + "\t| " + fullName3 + "\t| " + ccs2207_3 + "\t\t| " + ccs2208_3 + "\t\t| " + ccs2209_3 + "\t\t| " + ccs2210_3 + "\t\t| " + ccs2211_3 + "\t\t| " + ccs2215_3 + "\t\t| " + igs2202_3 + "\t\t| " + total_3 + "\t\t| " + average_3 + "\t\t| \t\t| \t\t|");
+        System.out.println("| " + regNumber4 + "\t| " + fullName4 + "\t| " + ccs2207_4 + "\t\t| " + ccs2208_4 + "\t\t| " + ccs2209_4 + "\t\t| " + ccs2210_4 + "\t\t| " + ccs2211_4 + "\t\t| " + ccs2215_4 + "\t\t| " + igs2202_4 + "\t\t| " + total_4 + "\t\t| " + average_4 + "\t\t| \t\t| \t\t|");
+        System.out.println("| " + regNumber5 + "\t| " + fullName5 + "\t| " + ccs2207_5 + "\t\t| " + ccs2208_5 + "\t\t| " + ccs2209_5 + "\t\t| " + ccs2210_5 + "\t\t| " + ccs2211_5 + "\t\t| " + ccs2215_5 + "\t\t| " + igs2202_5 + "\t\t| " + total_5 + "\t\t| " + average_5 + "\t\t| \t\t| \t\t|");
+        System.out.println("=================================================================================================================================================================================================================================");
+    }
+}
